c3d80b0eb6c010e218b6b8209c04b2d09e4c5997
roadfor1mcredits1 <44210726+roadfor1mcredits1@users.noreply.github.com>
2018-10-26 11:46:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,30 +3,22 @@ var ChannelID = "502893057973157890";// اي دي الروم
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = 's';
 
-client.on('warn', console.warn);
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-client.on('error', console.error);
-// Codes server - !.Zeyad' , ♚Roýale#5555
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
+  let args = message.content.split(" ").slice(1);
 
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 500);
-
+  if (command == "ay") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
